a9628b473ccfa61beea5f028fdbc0e9b40d60ee3
nnabicse <nnabi.cse@gmail.com>
2022-02-17 03:45:17 +0600

    javascript code complete initially

--- js/app.js
+++ js/app.js
@@ -1,43 +1,55 @@
-document.getElementById("calculate-button").addEventListener("click", function(){
+function calculateIncome() {
     const incomeField =  document.getElementById("income");
     const incomeAmount = parseInt(incomeField.value);
+    return incomeAmount;   
+}
+
+function calculateExpenses(){
     const foodField = document.getElementById("food");
     const foodAmount = parseInt(foodField.value);
     const rentField = document.getElementById("rent");
     const rentAmount = parseInt(rentField.value);
     const clothesField = document.getElementById("clothes");
     const clothesAmount = parseInt(clothesField.value);
-
     const totalExpense = foodAmount+rentAmount+clothesAmount;
-    const balance = incomeAmount - totalExpense;
-
-    document.getElementById("total-expenses").innerText  = document.getElementById("total-expenses").innerText + totalExpense;
-    
-    document.getElementById("balance").innerText  = document.getElementById("balance").innerText + balance;
-
     return totalExpense;
+}
+
+
+function calCulateBalance(){
+    const balance = calculateIncome() - calculateExpenses();
+    return balance;
+}
+
+function calculateSaving(){
+    const savingField = document.getElementById("saving-amount");
+    const savingAmount = parseInt(savingField.value);
+    const savedAmount = (calculateIncome()/100)*savingAmount;
+    return savedAmount;
+}
+
+function calculateRemaining(){
+    const remaining =  calculateIncome() - (calculateExpenses()+calculateSaving());
+    return remaining;
+}
+
+
+
+
+
+
+document.getElementById("calculate-button").addEventListener("click", function(){
+
+    document.getElementById("total-expenses").innerText  = document.getElementById("total-expenses").innerText + calculateExpenses();
+    
+    document.getElementById("balance").innerText  = document.getElementById("balance").innerText + calCulateBalance();
 
 })
 
 document.getElementById("saving").addEventListener("click", function(){
-    const savingField = document.getElementById("saving-amount");
-    const savingAmount = parseInt(savingField.value);
-    const incomeField =  document.getElementById("income");
-    const incomeAmount = parseInt(incomeField.value);
-    const foodField = document.getElementById("food");
-    const foodAmount = parseInt(foodField.value);
-    const rentField = document.getElementById("rent");
-    const rentAmount = parseInt(rentField.value);
-    const clothesField = document.getElementById("clothes");
-    const clothesAmount = parseInt(clothesField.value);
+    document.getElementById("total-saving").innerText  = document.getElementById("total-saving").innerText + calculateSaving();
 
-    const totalExpense = foodAmount+rentAmount+clothesAmount;
-
-    const savedAmount = (incomeAmount/100)*savingAmount;
-    const remainingAmount = incomeAmount - (totalExpense+savedAmount);
-    document.getElementById("total-saving").innerText  = document.getElementById("total-saving").innerText + savedAmount;
-
-    document.getElementById("remaining-amount").innerText  = document.getElementById("remaining-amount").innerText + remainingAmount;
+    document.getElementById("remaining-amount").innerText  = document.getElementById("remaining-amount").innerText + calculateRemaining();
     
 
 
